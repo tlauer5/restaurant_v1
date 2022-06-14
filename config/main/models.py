6ebb4e386814984107f98f4115c5015f5bfb2e7a
Tim <lauertim@gmx.de>
2022-06-14 19:45:18 +0200

    update

--- config/main/models.py
+++ config/main/models.py
@@ -4,5 +4,26 @@ from django.db import models
 class Order(models.Model):
     status_order = models.BooleanField()
 
+#Model Waiter
+class Waiter(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 #Model Customer
+class Customer(models.Model):
+    billed = models.BooleanField()
+    waiter = models.ForeignKey(Waiter)
+
+
+
+
+# in Class Customer
+#waiter = models.ForeignKey(Waiter, on_delete=models.CASCADE) -> wenn man zugeordneten
+# Waiter löscht dann löscht man auch alle Customer die diesen Waiter als Waiter hatten
+
+#es gibt noch statt ForeignKey OneToOne und ManyToMany (brauchen wir das auch???) -> wahrscheinlich ja bei Waiter
+
+#https://ilovedjango.com/django/models-and-databases/foreign-keys-on_delete-option-in-django-models/
 
