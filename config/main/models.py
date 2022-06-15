456bdc8becf8f4f8aad8175905ac60b25fd0156b
Tim <lauertim@gmx.de>
2022-06-15 09:30:16 +0200

    update

--- config/main/models.py
+++ config/main/models.py
@@ -1,23 +1,48 @@
 from django.db import models
 
-#Model Order
-class Order(models.Model):
-    status_order = models.BooleanField()
-
 #Model Waiter
 class Waiter(models.Model):
-    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.surname + ' ' + self.lastname
 
-#Model Customer
-class Customer(models.Model):
-    billed = models.BooleanField()
+#Model Chef
+class Chef(models.Model):
+    surname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.surname + ' ' + self.lastname
+
+#Model Table
+class Table(models.Model):
+    waiter = models.ForeignKey(Waiter) #??????
+    table_number = models.IntegerField()
+
+
+#Model Order
+class Order(models.Model):
     waiter = models.ForeignKey(Waiter)
+    chef = models.ForeignKey(Waiter)
+    table = models.ForeignKey(Table)
+    timestamp = models.DateTimeField()
+    cooked = models.BooleanField()
+    delivered = models.BooleanField()
+    billed = models.BooleanField()
+    dish_ID = models.IntegerField()
+    extra_wishes = models.CharField(max_length=250)
 
+    def update_cooked(self, status):
+        cooked = status
 
-
+#Model Reservation
+class Reservation(models.Model):
+    waiter = models.ForeignKey(Waiter)
+    customer_name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    table = models.ForeignKey(Table)
 
 # in Class Customer
 #waiter = models.ForeignKey(Waiter, on_delete=models.CASCADE) -> wenn man zugeordneten
