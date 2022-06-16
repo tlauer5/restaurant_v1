82931011b8d2489585b75a594b94720478b46260
Tim <lauertim@gmx.de>
2022-06-16 13:49:02 +0200

    update

--- config/main/tests.py
+++ config/main/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.utils import dateparse
+from .models import *
 
-# Create your tests here.
+ALL_FIXTURES = [
+    "waiters.json"
+]
+
+class WaitersTests(TestCase):
+    fixtures = ALL_FIXTURES
+
+    def test_waiter_print(self):
+        first_waiter = Waiter.objects.get(pk=1)
+        print(first_waiter)
+
+    def test_waiter_print2(self):
+        first_waiter = Waiter.objects.get(pk=1)
+        self.assertTrue(first_waiter.__str__())
+
+
+    def test_waiter_assertEqual(self):
+        first_waiter = Waiter.objects.get(pk=2)
+        self.assertEqual(first_waiter.surname, "Limbach")
+
+
+
+#https://docs.djangoproject.com/en/4.0/topics/testing/overview/
