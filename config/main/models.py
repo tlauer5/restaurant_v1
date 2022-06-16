f74d7025e41063563dab449f1afa6af4345447c6
Tim <lauertim@gmx.de>
2022-06-16 21:48:45 +0200

    update

--- config/main/models.py
+++ config/main/models.py
@@ -3,35 +3,35 @@ from enum import Enum
 from django.db import models
 
 
-#Model Waiter
+# Model Waiter
 class Waiter(models.Model):
     surname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
 
     def __str__(self):
-        return '\nSurname: ' + self.surname + '\nLastname: ' + self.lastname + '\n\n'
+        return self.surname + ' ' + self.lastname
 
-#Model Chef
+
+# Model Chef
 class Chef(models.Model):
-    surname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100, default='')
+    lastname = models.CharField(max_length=100, default='')
 
     def __str__(self):
-        return '\nSurname: ' + self.surname + '\nLastname: ' + self.lastname + '\n\n'
+        return self.surname + ' ' + self.lastname
 
-#Model Table
+# Model Table
 class Table(models.Model):
     table_number = models.IntegerField(default=None, null=True)
     places = models.IntegerField(default=None, null=True)
 
     def __str__(self):
-        return '\nTable: ' + self.table_number+ '\n\n'
+        return '\nTable: ' + self.table_number + '\n\n'
 
 
-#Model Order
+# Model Order
 class Order(models.Model):
-
-    class OrderStatus(models.TextChoices): #Alternativ mit IntegerChoices
+    class OrderStatus(models.TextChoices):  # Alternativ mit IntegerChoices
         UNASSIGNED = "unassigned"
         IN_PROGRESS = "in_progress"
         COOKED = "cooked"
@@ -42,33 +42,54 @@ class Order(models.Model):
     table = models.ForeignKey(Table, on_delete=models.SET_NULL, default=None, null=True)
     chef = models.ForeignKey(Chef, on_delete=models.SET_NULL, default=None, blank=True, null=True)
     timestamp = models.DateTimeField(default=None)
-    status = models.CharField(max_length=20, default=OrderStatus.UNASSIGNED)
+    status = models.CharField(max_length=20, choices=OrderStatus.choices, default=OrderStatus.UNASSIGNED)
     dish_ID = models.IntegerField(default=-1)
     extra_wishes = models.CharField(max_length=250, default=None, blank=True, null=True)
 
-    def __str__(self):
-        return 'Status: ' + self.OrderStatus + '\nDish: ' + str(self.dish_ID) + '\nExtra wishes: ' + self.extra_wishes + '\n\n'
+    def info_for_chef(self):
+        return '\nStatus: ' + self.get_status_display() + \
+               '\nChef: ' + str(self.chef) + \
+               '\nDish: ' + str(self.dish_ID) + \
+               '\nExtra wishes: ' + str(self.extra_wishes) + \
+               '\n\n'
 
-    def getTableNumber(self, pk):
-        return self.table
+    def info_for_waiter(self):
+        return '\nStatus: ' + self.get_status_display() + \
+               '\nWaiter: ' + str(self.chef) + \
+               '\nTable Number: ' + str(self.table.table_number) + \
+               '\nDish: ' + str(self.dish_ID) + \
+               '\nExtra wishes: ' + str(self.extra_wishes) + \
+               '\n\n'
 
-#Model Reservation
+# Model Reservation
 class Reservation(models.Model):
     customer_name = models.CharField(max_length=100)
     timestamp = models.DateTimeField(default=None, null=True)
     table = models.ForeignKey(Table, on_delete=models.SET_NULL, null=True)
     places = models.IntegerField(default=None, null=True)
 
-#es gibt noch statt ForeignKey OneToOne und ManyToMany (brauchen wir das auch???) -> wahrscheinlich ja bei Waiter
+# es gibt noch statt ForeignKey OneToOne und ManyToMany (brauchen wir das auch???) -> wahrscheinlich ja bei Waiter
 
-#https://ilovedjango.com/django/models-and-databases/foreign-keys-on_delete-option-in-django-models/
-#https://stackoverflow.com/questions/54802616/how-to-use-enums-as-a-choice-field-in-django-model
-#https://stackoverflow.com/questions/16046478/django-model-nullable-field
+# https://ilovedjango.com/django/models-and-databases/foreign-keys-on_delete-option-in-django-models/
+# https://stackoverflow.com/questions/54802616/how-to-use-enums-as-a-choice-field-in-django-model
+# https://stackoverflow.com/questions/16046478/django-model-nullable-field
 
-#manytomany -> tabelle
-#https://www.sankalpjonna.com/learn-django/the-right-way-to-use-a-manytomanyfield-in-django
-#https://zerotobyte.com/django-many-to-many-relationship-explained/
-#https://books.agiliq.com/projects/django-orm-cookbook/en/latest/one_to_many.html
-#https://www.reddit.com/r/django/comments/l937f1/the_right_way_to_use_a_manytomanyfield_in_django/
+# manytomany -> tabelle
+# https://www.sankalpjonna.com/learn-django/the-right-way-to-use-a-manytomanyfield-in-django
+# https://zerotobyte.com/django-many-to-many-relationship-explained/
+# https://books.agiliq.com/projects/django-orm-cookbook/en/latest/one_to_many.html
+# https://www.reddit.com/r/django/comments/l937f1/the_right_way_to_use_a_manytomanyfield_in_django/
 
-#query bei abfrage in tests
+# query bei abfrage in tests
+
+#textchoices Ausschreiben (get_status_display())
+#https://stackoverflow.com/questions/59606682/how-to-get-textchoice-enum-value-in-str-method
+
+#an value von foreign key rankommen:
+#self.table.table_number
+
+#wenn man TextChoices hat dann gibt es immer eine methode get_FOO_display() mit der der String von dem Jeweiligen Choice ausgegeben werden kann
+
+#wenn man Objekt ausgeben möchte was eventuell noch nicht belegt ist, dann einfach __str__ Methode definieren (und so belegen
+# dass wenn ein Objekt ausgegeben werden soll was schon definiert ist auch sauber aussieht) und dort
+#wo man Objekt (none) ausgeben möchte mit str(object) aufrufen
--- config/main/tests.py
+++ config/main/tests.py
@@ -32,29 +32,55 @@ class WaitersTests(TestCase):
     def test_showAllOrders(self):
         waiter_pk = 1
         unassignedOrders = Order.objects.filter(waiter=waiter_pk, status=Order.OrderStatus.UNASSIGNED)
-        inProgressOrders = Order.objects.filter(chef=waiter_pk, status=Order.OrderStatus.IN_PROGRESS)
-        cookedOrders = Order.objects.filter(chef=waiter_pk, status=Order.OrderStatus.COOKED)
-        deliveredOrders = Order.objects.filter(chef=waiter_pk, status=Order.OrderStatus.DELIVERED)
-        billedOrders = Order.objects.filter(chef=waiter_pk, status=Order.OrderStatus.BILLED)
+        inProgressOrders = Order.objects.filter(waiter=waiter_pk, status=Order.OrderStatus.IN_PROGRESS)
+        cookedOrders = Order.objects.filter(status=Order.OrderStatus.COOKED)
+        deliveredOrders = Order.objects.filter(waiter=waiter_pk, status=Order.OrderStatus.DELIVERED)
+        billedOrders = Order.objects.filter(waiter=waiter_pk, status=Order.OrderStatus.BILLED)
 
+        for order in cookedOrders:
+            print(order.info_for_waiter())
 
-
-    def test_deleteOrder(self):
-        pass
+    def test_delete_order(self):
+        return
 
 
 class ChefsTests(TestCase):
     fixtures = ALL_FIXTURES
 
-    def test_myOpenOrders(self):
-        chef_pk = 2
-        for order in Order.objects.filter(chef=chef_pk, status=Order.OrderStatus.IN_PROGRESS):
-            print(order)
+    def test_show_unassigned_orders(self):
+
+        unassigned_orders = Order.objects.filter(status=Order.OrderStatus.UNASSIGNED)
+
+        for order in unassigned_orders:
+            print(order.info_for_chef())
+
+    def test_assign_order_to_chef(self):
+        new_order_pk = 1
+
+        unassigned_orders = Order.objects.filter(status=Order.OrderStatus.UNASSIGNED)
+
+        if not unassigned_orders.exists():
+            return
+
+        order_to_assign_pk = unassigned_orders[0].pk
+
+        print('PK: ' + str(order_to_assign_pk) + '\n')
+
+        # Current PK and Status
+        print(Order.objects.filter(pk=order_to_assign_pk)[0].info_for_chef())
+
+        # Update PK and Status
+        Order.objects.filter(pk=order_to_assign_pk).update(chef=new_order_pk, status=Order.OrderStatus.IN_PROGRESS)
+
+        # Updated PK and Status
+        print(Order.objects.filter(pk=order_to_assign_pk)[0].info_for_chef())
+
+
 
     def test_getNewOrder(self):
         pass
 
-    def test_updateOrder(self):
+    def test_update_order(self):
         pass
 
         #for order in qSet:
@@ -69,3 +95,7 @@ class ChefsTests(TestCase):
 
 
 #https://docs.djangoproject.com/en/4.0/topics/testing/overview/
+
+#pk über namen herausfinden
+#new_chef_pk = Chef.objects.filter(surname="Kurt", lastname="Lamm")[0].pk
+#am besten noch prüfen ob nur einer in Liste ist oder ob überhaupt jemand mit dem Namen gefunden wurde
