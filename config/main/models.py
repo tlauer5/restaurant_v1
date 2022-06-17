8679f101404aaa12a93a98c0165b2a5c7aa328ab
Tim <lauertim@gmx.de>
2022-06-17 16:24:14 +0200

    update

--- config/main/models.py
+++ config/main/models.py
@@ -1,5 +1,3 @@
-from enum import Enum
-
 from django.db import models
 
 
@@ -9,7 +7,7 @@ class Waiter(models.Model):
     lastname = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.surname + ' ' + self.lastname
+        return str(self.surname) + ' ' + str(self.lastname)
 
 
 # Model Chef
@@ -18,15 +16,16 @@ class Chef(models.Model):
     lastname = models.CharField(max_length=100, default='')
 
     def __str__(self):
-        return self.surname + ' ' + self.lastname
+        return str(self.surname) + ' ' + str(self.lastname)
+
 
 # Model Table
 class Table(models.Model):
-    table_number = models.IntegerField(default=None, null=True)
+    tableNumber = models.IntegerField(default=None, null=True)
     places = models.IntegerField(default=None, null=True)
 
     def __str__(self):
-        return '\nTable: ' + str(self.table_number) + '\n\n'
+        return '\nTable: ' + str(self.tableNumber) + '\n\n'
 
 
 # Model Order
@@ -43,27 +42,28 @@ class Order(models.Model):
     chef = models.ForeignKey(Chef, on_delete=models.SET_NULL, default=None, blank=True, null=True)
     timestamp = models.DateTimeField(default=None)
     status = models.CharField(max_length=20, choices=OrderStatus.choices, default=OrderStatus.UNASSIGNED)
-    dish_ID = models.IntegerField(default=-1)
-    extra_wishes = models.CharField(max_length=250, default=None, blank=True, null=True)
+    dishId = models.IntegerField(default=-1)
+    extraWishes = models.CharField(max_length=250, default=None, blank=True, null=True)
 
     def info_for_chef(self):
         return '\nStatus: ' + self.get_status_display() + \
                '\nChef: ' + str(self.chef) + \
-               '\nDish: ' + str(self.dish_ID) + \
-               '\nExtra wishes: ' + str(self.extra_wishes) + \
+               '\nDish: ' + str(self.dishId) + \
+               '\nExtra wishes: ' + str(self.extraWishes) + \
                '\n\n'
 
     def info_for_waiter(self):
         return '\nStatus: ' + self.get_status_display() + \
                '\nWaiter: ' + str(self.waiter) + \
-               '\nTable Number: ' + str(self.table.table_number) + \
-               '\nDish: ' + str(self.dish_ID) + \
-               '\nExtra wishes: ' + str(self.extra_wishes) + \
+               '\nTable Number: ' + str(self.table.tableNumber) + \
+               '\nDish: ' + str(self.dishId) + \
+               '\nExtra wishes: ' + str(self.extraWishes) + \
                '\n\n'
+
 
 # Model Reservation
 class Reservation(models.Model):
-    customer_name = models.CharField(max_length=100)
+    customerName = models.CharField(max_length=100)
     timestamp = models.DateTimeField(default=None, null=True)
     table = models.ManyToManyField(Table)
     places = models.IntegerField(default=None, null=True)
@@ -75,8 +75,8 @@ class Reservation(models.Model):
                '\ntable(s): ' + str(self.extractTableNumbers())
 
     def extractTableNumbers(self):
-        #function which extracts only the table numbers from a queryset and returns them as string
-        qSet = Reservation.objects.get(pk=self.pk).table.all() #table.all() gets manytomany values
+        # function which extracts only the table numbers from a queryset and returns them as string
+        qSet = Reservation.objects.get(pk=self.pk).table.all()  # table.all() gets manytomany values
 
         stringWithTableNumbers = ''
 
@@ -87,36 +87,3 @@ class Reservation(models.Model):
         stringWithTableNumbers = stringWithTableNumbers[:-2]
 
         return stringWithTableNumbers
-
-
-
-
-# es gibt noch statt ForeignKey OneToOne und ManyToMany (brauchen wir das auch???) -> wahrscheinlich ja bei Waiter
-
-# https://ilovedjango.com/django/models-and-databases/foreign-keys-on_delete-option-in-django-models/
-# https://stackoverflow.com/questions/54802616/how-to-use-enums-as-a-choice-field-in-django-model
-# https://stackoverflow.com/questions/16046478/django-model-nullable-field
-# https://stackoverflow.com/questions/7655438/how-do-django-fixtures-handle-manytomanyfields
-
-# manytomany
-# reservations auf table ist manytomany -> dann muss man alle table in reservations.json als Liste angeben []
-# ein table kann schon allein mit foreign key mehrere reservations haben... aber eine reservation bisher nur ein tisch
-# mit manytomanyfield(table) -> können beide Objekte in beide Richtungen mehrere Objekte haben
-# https://www.sankalpjonna.com/learn-django/the-right-way-to-use-a-manytomanyfield-in-django
-# https://zerotobyte.com/django-many-to-many-relationship-explained/
-# https://books.agiliq.com/projects/django-orm-cookbook/en/latest/one_to_many.html
-# https://www.reddit.com/r/django/comments/l937f1/the_right_way_to_use_a_manytomanyfield_in_django/
-
-# query bei abfrage in tests
-
-#textchoices Ausschreiben (get_status_display())
-#https://stackoverflow.com/questions/59606682/how-to-get-textchoice-enum-value-in-str-method
-
-#an value von foreign key rankommen:
-#self.table.table_number
-
-#wenn man TextChoices hat dann gibt es immer eine methode get_FOO_display() mit der der String von dem Jeweiligen Choice ausgegeben werden kann
-
-#wenn man Objekt ausgeben möchte was eventuell noch nicht belegt ist, dann einfach __str__ Methode definieren (und so belegen
-# dass wenn ein Objekt ausgegeben werden soll was schon definiert ist auch sauber aussieht) und dort
-#wo man Objekt (none) ausgeben möchte mit str(object) aufrufen
--- config/main/tests.py
+++ config/main/tests.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
 from django.test import TestCase
-from django.utils import dateparse
 from .models import *
 
 ALL_FIXTURES = [
@@ -16,28 +14,27 @@ class WaitersTests(TestCase):
     fixtures = ALL_FIXTURES
 
     def test_create_order(self):
-        # Number of Orders before
-        all_orders = Order.objects.filter()
+        allOrders = Order.objects.filter()
 
-        if not all_orders.exists():
+        if not allOrders.exists():
             return
 
-        number_of_orders_before = all_orders.count()
+        numberOfOrdersBefore = allOrders.count()
 
         Order.objects.create(waiter_id=3,
                              table_id=13,
                              chef_id=None,
                              timestamp=datetime.now(),
                              status=Order.OrderStatus.UNASSIGNED,
-                             dish_ID=63,
-                             extra_wishes='extra pepper')
+                             dishId=63,
+                             extraWishes='extra pepper')
 
-        number_of_orders_after = all_orders.count()
+        numberOfOrdersAfter = allOrders.count()
 
-        if number_of_orders_after > number_of_orders_before:
-            print('\n\nSuccessfully created new order!\n' + \
-                  'Orders before: ' + str(number_of_orders_before) + \
-                  ' - Orders after: ' + str(number_of_orders_after))
+        if numberOfOrdersAfter > numberOfOrdersBefore:
+            print('\n\nSuccessfully created new order!\n' +
+                  'Orders before: ' + str(numberOfOrdersBefore) +
+                  ' - Orders after: ' + str(numberOfOrdersAfter))
 
     def test_show_cooked_orders(self):
         cookedOrders = Order.objects.filter(status=Order.OrderStatus.COOKED)
@@ -46,24 +43,23 @@ class WaitersTests(TestCase):
             print(order.info_for_waiter())
 
     def test_delete_order(self):
-        # Number of Orders before
-        all_orders = Order.objects.filter()
+        allOrders = Order.objects.filter()
 
-        if not all_orders.exists():
+        if not allOrders.exists():
             return
 
-        number_of_orders_before = all_orders.count()
+        numberOfOrdersBefore = allOrders.count()
 
         orderToDeletePk = 4
 
         Order.objects.filter(pk=orderToDeletePk).delete()
 
-        number_of_orders_after = all_orders.count()
+        numberOfOrdersAfter = allOrders.count()
 
-        if number_of_orders_after < number_of_orders_before:
-            print('\n\nSuccessfully deleted an order!\n' + \
-                  'Orders before: ' + str(number_of_orders_before) + \
-                  ' - Orders after: ' + str(number_of_orders_after))
+        if numberOfOrdersAfter < numberOfOrdersBefore:
+            print('\n\nSuccessfully deleted an order!\n' +
+                  'Orders before: ' + str(numberOfOrdersBefore) +
+                  ' - Orders after: ' + str(numberOfOrdersAfter))
 
     def test_create_reservation(self):
         allReservations = Reservation.objects.filter()
@@ -74,57 +70,54 @@ class WaitersTests(TestCase):
         numberOfReservationsBefore = allReservations.count()
 
         firstTablePk, secondTablePk= 15, 16
-        newReservation = Reservation.objects.create(customer_name='Mayer',
+
+        newReservation = Reservation.objects.create(customerName='Mayer',
                                                     timestamp="2022-05-27T18:00:00.00Z",
                                                     places=8)
 
         newReservation.table.set([firstTablePk, secondTablePk])
-        #oder auch mit .add(15) -> man muss erst Instanz erstellen bevor man many to many field beschreiben kann...
-
-        #print(newReservation)
 
         numberOfReservationsAfter = allReservations.count()
 
         if numberOfReservationsAfter > numberOfReservationsBefore:
-            print('\n\nSuccessfully created new order!\n' + \
-                  'Orders before: ' + str(numberOfReservationsBefore) + \
+            print('\n\nSuccessfully created new order!\n' +
+                  'Orders before: ' + str(numberOfReservationsBefore) +
                   ' - Orders after: ' + str(numberOfReservationsAfter))
+
 
 class ChefsTests(TestCase):
     fixtures = ALL_FIXTURES
 
     def test_show_unassigned_orders(self):
+        unassignedOrders = Order.objects.filter(status=Order.OrderStatus.UNASSIGNED)
 
-        unassigned_orders = Order.objects.filter(status=Order.OrderStatus.UNASSIGNED)
-
-        for order in unassigned_orders:
+        for order in unassignedOrders:
             print(order.info_for_chef())
 
     def test_assign_order_to_chef(self):
-        new_order_pk = 1
+        newOrderPk = 1
 
-        unassigned_orders = Order.objects.filter(status=Order.OrderStatus.UNASSIGNED)
+        unassignedOrders = Order.objects.filter(status=Order.OrderStatus.UNASSIGNED)
 
-        if not unassigned_orders.exists():
+        if not unassignedOrders.exists():
             return
 
-        order_to_assign_pk = unassigned_orders[0].pk
+        orderToAssignPk = unassignedOrders[0].pk
 
         # Current PK and Status
-        print(Order.objects.filter(pk=order_to_assign_pk)[0].info_for_chef())
+        print(Order.objects.filter(pk=orderToAssignPk)[0].info_for_chef())
 
         # Update PK and Status
-        Order.objects.filter(pk=order_to_assign_pk).update(chef=new_order_pk, status=Order.OrderStatus.IN_PROGRESS)
+        Order.objects.filter(pk=orderToAssignPk).update(chef=newOrderPk, status=Order.OrderStatus.IN_PROGRESS)
 
         # Updated PK and Status
-        print(Order.objects.filter(pk=order_to_assign_pk)[0].info_for_chef())
+        print(Order.objects.filter(pk=orderToAssignPk)[0].info_for_chef())
 
 
 class AdminsTest(TestCase):
     fixtures = ALL_FIXTURES
 
     def test_admin_create_new_waiter(self):
-        # Number of Waiters before
         allWaiters = Waiter.objects.filter()
 
         numberOfWaitersBefore = allWaiters.count()
@@ -138,27 +131,6 @@ class AdminsTest(TestCase):
         numberOfWaitersAfter = allWaiters.count()
 
         if numberOfWaitersAfter > numberOfWaitersBefore:
-            print('\n\nSuccessfully created new order!\n' + \
-                  'Waiters before: ' + str(numberOfWaitersBefore) + \
+            print('\n\nSuccessfully created new order!\n' +
+                  'Waiters before: ' + str(numberOfWaitersBefore) +
                   ' - Waiters after: ' + str(numberOfWaitersAfter))
-
-
-
-
-
-# https://docs.djangoproject.com/en/4.0/topics/testing/overview/
-
-# pk oder anderes über namen herausfinden -> man erhält von filter ein queryset zurück (darüber kann man iterieren (for order in querySetOrder:..))
-# new_chef_pk = Chef.objects.filter(surname="Kurt", lastname="Lamm")[0].pk
-# am besten noch prüfen ob nur einer in Liste ist oder ob überhaupt jemand mit dem Namen gefunden wurde
-
-# assertEqual zum Vergleich
-
-
-# mit objects.get erhält man Objekt zurück das darauf passt (Achtung: nur eins -sonst fehler)
-# Order.objects.get(pk=2)
-# Order.objects.get(dish_ID=2)
-
-
-# neues Objekt anlegen welches auf foreignKeys zurückgreift -> mit foreignkey_id
-# new_order = Order.objects.create(waiter_id=3) ->waiter ist foreignkey....
